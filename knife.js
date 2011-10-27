fae77b1bcd8beb8e4f0f1f14a76732b1ff38d1cd
Cam Pedersen <diffference@gmail.com>
2011-10-27 19:56:29 -0400

    IT LIVES

--- knife.js
+++ knife.js
@@ -1,29 +1,44 @@
 module.exports = {
-  parse :function knife(shit){
+  parse: function(shit, debug){
     if (typeof shit == 'object') shit = JSON.stringify(shit);
     if (typeof shit == 'array') shit = shit.join('');
 
-    var shits = [];
-
-    for (var i in shit) {
-      var sculpture;
-      if (shit[i] == '}') {
-          console.log(shit.substr(0, i));
-        try {
-          sculpture = JSON.parse(shit.substr(0, i));
-          console.log(sculpture);
-        } catch (e) {
-          console.log('Coult not parse:');
-          console.log(shit);
-        }
-      }
-      if (sculpture) {
-        shit.splice(0, i);
-        shits.push(sculpture);
+    var easy;
+    try {
+      easy = JSON.parse(shit);
+    } catch (e) {}
+    if (easy) {
+      return {
+        result: [ easy ],
+        remainerd: null
       }
     }
 
-    return shits;
+    var shits = [],
+        i = 0;
+
+    while (i < shit.length) {
+      var sculpture = undefined;
+      if (debug) console.log(shit.substr(0, i + 1));
+      try {
+        sculpture = JSON.parse(shit.substr(0, i + 1));
+        if (debug) console.log(sculpture);
+      } catch (e) { }
+      if (sculpture) {
+        shits.push(sculpture);
+        var shitleft = shit.substr(i + 1, shit.length - 1);
+        if (debug) console.log('---------------- ' + shitleft);
+        shit = shitleft;
+        i = 0;
+      } else {
+        i++;
+      }
+    }
+
+    return {
+      result: shits,
+      remainder: shit
+    };
 
   }
 
