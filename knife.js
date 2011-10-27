99437a4cfa31b7c6737e258267c20c5383edf21b
Cam Pedersen <diffference@gmail.com>
2011-10-27 19:22:29 -0400

    initial parser

---
+++ knife.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parse :function knife(shit){
+    if (typeof shit == 'object') shit = JSON.stringify(shit);
+    if (typeof shit == 'array') shit = shit.join('');
+
+    var shits = [];
+
+    for (var i in shit) {
+      var sculpture;
+      if (shit[i] == '}') {
+        try {
+          sculpture = JSON.parse(shit.slice(0, i));
+          console.log(sculpture);
+        } catch (e) {
+          console.log('Coult not parse:');
+          console.log(shit);
+        }
+      }
+      if (sculpture) {
+        shit.splice(0, i);
+        shits.push(sculpture);
+      }
+    }
+
+    return shits;
+
+  }
+
+}
